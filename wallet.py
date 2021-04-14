01ff46af1a03dbf32164ac16662114ee998f0e4d
Matt Houk <72565963+matthouk@users.noreply.github.com>
2021-04-13 21:59:01 -0700

    added init stuff and wallet.py, constants.py etc

---
+++ wallet.py
@@ -0,0 +1,14 @@
+import subprocess
+import json
+import os
+
+command = './derive -g --mnemonic=mnemonic --numderive=3 --coin=ETH --cols=path,address,privkey,pubkey --format=json'
+
+
+mnemonic = os.getenv('MNEMONIC-HW')
+p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+output, err = p.communicate()
+p_status = p.wait()
+
+keys = json.loads(output)
+print(keys)
