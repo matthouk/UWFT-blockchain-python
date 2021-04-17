a02e8f7542f7186ef2c129d5783211c02710f0f1
Matt Houk <72565963+matthouk@users.noreply.github.com>
2021-04-17 12:24:26 -0700

    Web3 and PoA framework

--- wallet.py
+++ wallet.py
@@ -19,3 +19,58 @@ print(keys)
 # for i in range(5): 
 #    print(keys[i]['address'])
 
+
+
+# Web3 stuff: 
+
+from web3 import Web3
+from web3.middleware import geth_poa_middleware
+from eth_account import Account
+
+
+from pathlib import Path
+from getpass import getpass
+
+
+# LocalHost stuff here 
+3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+account_one = # this is where the "keys" should go 
+
+with open(
+    Path(
+        "" 
+    )
+) as keyfile:
+    encrypted_key = keyfile.read()
+    private_key = w3.eth.account.decrypt(
+        encrypted_key, getpass("Enter keystore password: ")
+    )
+    account_two = Account.from_key(private_key)
+
+
+# Creating a transaction 
+def create_raw_tx(account, recipient, amount):
+    gasEstimate = w3.eth.estimateGas(
+        {"from": account.address, "to": recipient, "value": amount}
+    )
+    return {
+        "from": account.address,
+        "to": recipient,
+        "value": amount,
+        "gasPrice": w3.eth.gasPrice,
+        "gas": gasEstimate,
+        "nonce": w3.eth.getTransactionCount(account.address),
+    }
+
+
+#Sending it 
+def send_tx(account, recipient, amount):
+    tx = create_raw_tx(account, recipient, amount)
+    signed_tx = account.sign_transaction(tx)
+    result = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    print(result.hex())
+    return result.hex()
+
+
